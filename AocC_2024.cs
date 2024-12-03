50f8dedc5627f95e5465b785c7856cdbae5572ef
LinusBroecker <41783995+LinusBroecker@users.noreply.github.com>
2024-12-03 22:45:46 +0100

    Day02

--- AocC_2024.cs
+++ AocC_2024.cs
@@ -3,7 +3,7 @@
     static void Main(string[] args) {
      
       new Day01("D:\\Programmierung\\AdventOfCode_2024\\Input\\day01.txt").Solve();
-
+      new Day02("D:\\Programmierung\\AdventOfCode_2024\\Input\\day02.txt").Solve();
       new Day03("D:\\Programmierung\\AdventOfCode_2024\\Input\\day03.txt").Solve();
       
       Console.ReadKey();
---
+++ Day02.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+
+namespace AdventOfCode_2024 {
+
+  public class Day02 {
+
+    private string path;
+    private List<List<int>> input;
+    private int partOne, partTwo;
+
+    public Day02(string path) {
+      this.path = path;
+    }
+
+    public void Solve() {
+      Stopwatch sw = Stopwatch.StartNew();
+      input = readInput(path);
+
+      solve();
+      sw.Stop();
+
+      Console.WriteLine($"Day02: {sw.ElapsedMilliseconds} ms");
+      Console.WriteLine("Part One: " + partOne);
+      Console.WriteLine("Part Two: " + partTwo);
+      Console.WriteLine("------------------------------------------------------------");
+    }
+
+    private List<List<int>> readInput(string path) {
+      List<List<int>> result = new();
+      string[] lines = File.ReadAllLines(path);
+
+      foreach(string l in lines) {
+        string[] split = l.Split(" ");
+
+        List<int> li = new();
+        foreach(string s in split) {
+          li.Add(int.Parse(s));
+        }
+
+        result.Add(li);
+      }
+
+      return result;
+    }
+
+    private void solve() {
+      foreach(List<int> l in input) {
+        if (isSafe(l)) {
+          partOne++;
+        }
+        else { // Nicht schön:(
+          for(int i = 0; i < l.Count; i++) {
+            List<int> tmp = new List<int>(l); tmp.RemoveAt(i);
+            
+            if(isSafe(tmp)) {
+              partTwo++;
+              break;
+            }
+          }
+        }
+      }
+
+      partTwo += partOne;
+    }
+
+    private bool isSafe(List<int> li) {
+      bool decrease = li[0] > li[1] ? true : false;
+
+      for (int i = 0; i < li.Count - 1; i++) {
+        if (decrease) {
+          if (li[i + 1] >= li[i]) return false;
+          if (li[i + 1] < li[i] - 3) return false;
+        }
+        else {
+          if (li[i + 1] <= li[i]) return false;
+          if (li[i + 1] > li[i] + 3) return false;
+        }
+      }
+
+      return true;
+    }
+  }
+
+}
